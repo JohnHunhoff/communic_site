a23dfbbfa77fef9899701de863cc5bcb3be6fa8d
john <johnhunhoff@gmail.com>
2020-07-09 11:28:20 -0300

    finish

--- communic/settings.py
+++ communic/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0f=1t0%e!8rm(2qj=5yp0#&akm%p^i1kw$&w_2_2jf3r#tf2c6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
